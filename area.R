a8e1f2b1516a7de06cbe6cbb21ea3ff7bdaaf7df
Benjamin Stocker <benjaminstocker@Benjamins-iMac.local>
2017-11-21 18:40:59 +0100

    changed radius of the Earth in gridcell area calculation

--- area.R
+++ area.R
@@ -8,11 +8,8 @@
 ## dx and dy are in units of degrees
 
 area <- function(lat,dx=1,dy=1){
-  r_earth <- 6370000
-
-  area <- 4*r_earth^2*0.5*dx*pi/180*
-    cos(abs(lat)*pi/180)*
-    sin(0.5*dy*pi/180)
+  # r_earth <- 6370000
+  r_earth <- 6370499.317638  # to be consistent with how Ferret calculates areas of spheres (https://www.pmel.noaa.gov/maillists/tmap/ferret_users/fu_2016/msg00155.html) 
+  area <- 4 * r_earth^2 * 0.5 * dx * pi/180 * cos( abs(lat) * pi/180 ) * sin( 0.5 * dy * pi/180 )
   return(area)
 }
-
--- regrid_gpp_modis.sh
+++ regrid_gpp_modis.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-## generate weights file (is going to be used for all files to speed up the process)
-infil="./2015/MODIS-C06__MOD17A2-8-Daily-GPP__0.05deg__20150509__NTSG-UMT__UHAM-ICDC__fv0.01.nc"
-cdo genbil,~/data/landmasks/area_halfdeg.nc ${infil} weights_gpp_modis.nc
+# ## generate weights file (is going to be used for all files to speed up the process)
+# infil="./2015/MODIS-C06__MOD17A2-8-Daily-GPP__0.05deg__20150509__NTSG-UMT__UHAM-ICDC__fv0.01.nc"
+# cdo genbil,~/data/landmasks/area_halfdeg.nc ${infil} weights_gpp_modis.nc
 
-## remap all files
-list=`find . -name MODIS*.nc`
-for infil in ${list}
-do
-	outfil=${infil/0\.05deg/0\.5deg}
-	sudo cdo remap,~/data/landmasks/area_halfdeg.nc,weights_gpp_modis.nc ${infil} ${outfil}
-done
+# ## remap all files
+# list=`find . -name MODIS*.nc`
+# for infil in ${list}
+# do
+# 	outfil=${infil/0\.05deg/0\.5deg}
+# 	sudo cdo remap,~/data/landmasks/area_halfdeg.nc,weights_gpp_modis.nc ${infil} ${outfil}
+# done
 
 for iyr in {2000..2015}
 do
 	echo $iyr
 	
-	## combine all 0.5 deg files of each year into a single file
-	sudo cdo mergetime ${iyr}/MODIS-C06__MOD17A2-8-Daily-GPP__0.5deg__${iyr}????__NTSG-UMT__UHAM-ICDC__fv0.01.nc ${iyr}/MODIS-C06__MOD17A2-8-Daily-GPP__0.5deg__${iyr}__NTSG-UMT__UHAM-ICDC__fv0.01.nc
+	# ## combine all 0.5 deg files of each year into a single file
+	# sudo cdo mergetime ${iyr}/MODIS-C06__MOD17A2-8-Daily-GPP__0.5deg__${iyr}????__NTSG-UMT__UHAM-ICDC__fv0.01.nc ${iyr}/MODIS-C06__MOD17A2-8-Daily-GPP__0.5deg__${iyr}__NTSG-UMT__UHAM-ICDC__fv0.01.nc
 	
+	# ## Fill missing values along time axis
+	# Rscript fillmiss_modis.R
+
 	## get annual total GPP by summing over time dimension (total GPP in 8-day period is given, in kg C m-2)
-	sudo cdo yearsum ${iyr}/MODIS-C06__MOD17A2-8-Daily-GPP__0.5deg__${iyr}__NTSG-UMT__UHAM-ICDC__fv0.01.nc ${iyr}/MODIS-C06__MOD17A2-8-Daily-GPP__0.5deg__${iyr}__NTSG-UMT__UHAM-ICDC__fv0.01_ann.nc 
+	sudo cdo yearsum ${iyr}/MODIS-C06__MOD17A2-8-Daily-GPP__0.5deg__${iyr}__NTSG-UMT__UHAM-ICDC__fv0.01_FILLEDBYTIME.nc ${iyr}/MODIS-C06__MOD17A2-8-Daily-GPP__0.5deg__${iyr}__NTSG-UMT__UHAM-ICDC__fv0.01_ann.nc 
 
 done
 
