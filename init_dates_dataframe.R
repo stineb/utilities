27b88644a4b2b35e9a7db7bd242eca35dc46b9c8
Benjamin Stocker <benjaminstocker@Benjamins-iMac.local>
2017-12-20 18:58:50 +0100

    updated

--- init_dates_dataframe.R
+++ init_dates_dataframe.R
@@ -5,42 +5,19 @@ init_dates_dataframe <- function( yrstart, yrend, startmoy=1, startdoy=1, freq="
 
   if (freq=="days"){
 
+    startdate <- ymd( paste0( as.character(yrstart), "-", sprintf( "%02d", startmoy), "-01" ) ) + days( startdoy - 1 )
+    enddate   <- ymd( as.Date( paste0( as.character(yrend  ), "-", sprintf( "%02d", endmoy  ), "-", sprintf( "%02d", enddom  ) ) ) )
+    dates     <- seq( from = startdate, to = enddate, by = freq )
+
+    ddf <-  tibble( date=dates ) %>% 
+            mutate( ndayyear = ifelse( leap_year( date ), 366, 365 ) ) %>%
+            mutate( year_dec = year(date) + ( yday(date) - 1 ) / ndayyear,
+                    doy = yday(date) ) %>% 
+            select( -ndayyear )
+
     if (noleap){
 
-      ndaymonth <- c(31,28,31,30,31,30,31,31,30,31,30,31)
-      ndayyear <- sum(ndaymonth) 
-      nmonth   <- length(ndaymonth)
-      nyrs    <- length( yrstart:yrend )
-      dm   <- rep( NA, sum(ndaymonth)*length(yrstart:yrend) )
-      jdx <- 0
-      for (yr in yrstart:yrend ){
-        for (imoy in 1:nmonth){
-          for (idm in 1:ndaymonth[imoy]){
-            jdx <- jdx + 1 
-            dm[jdx]   <- idm
-          }
-        }
-      }
-      ddf <-  tibble( 
-                      doy=rep( seq(ndayyear), nyrs ), 
-                      moy=rep( rep( seq(nmonth), times=ndaymonth ), times=nyrs ),
-                      dom=dm,
-                      year=rep( yrstart:yrend, each=ndayyear )
-                      ) %>% 
-              mutate( year_dec = year + ( doy - 1 ) / 365,
-                      date = as.POSIXct( as.Date( paste0( as.character(year), "-", sprintf( "%02d", moy), "-", sprintf( "%02d", dom) ) ) ) )
-
-    } else {
-
-      startdate <- ymd( paste0( as.character(yrstart), "-", sprintf( "%02d", startmoy), "-01" ) ) + days( startdoy - 1 )
-      enddate   <- ymd( as.Date( paste0( as.character(yrend  ), "-", sprintf( "%02d", endmoy  ), "-", sprintf( "%02d", enddom  ) ) ) )
-      dates     <- seq( from = startdate, to = enddate, by = freq )
-
-      ddf <-  tibble( date=dates ) %>% 
-              mutate( ndayyear = ifelse( (year(date) %% 4) == 0, 366, 365  ) ) %>%
-              mutate( year_dec = year(date) + ( yday(date) - 1 ) / ndayyear,
-                      doy = yday(date) ) %>% 
-              select( -ndayyear )
+      ddf <- ddf %>% filter( !( month(date)==2 & mday(date)==29) )
 
     }
 
